1433eb5571c93ba93dfeb6e8737b2cd451f1f251
Alexandre Ménielle <alexandre.menielle@gmail.com>
2018-09-04 20:58:27 +0200

    auto create and delete auth

--- Functions/functions/index.js
+++ Functions/functions/index.js
@@ -4,17 +4,23 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 
-exports.autoCreateUser = functions.auth.user().onCreate(event => {
-
-  const user = event.data; // The Firebase user.
+exports.autoCreateUser = functions.auth.user().onCreate((user) => {
+  //functions.auth.user().onCreate(event => {
   const email = user.email; // The email of the user.
   const uid = user.uid;
 
   const collection = db.collection("users")
   collection.doc(uid).set({
     email : email,
-    uid : uid
+    id : uid
   }).then(()=>{
     console.log("User created");
+    return true;
   })
+});
+
+exports.autoDeleteUser = functions.auth.user().onDelete((user) => {
+  db.collection("users").doc(user.uid).delete();
+  console.log("User [" + user.email + ", " + user.uid + "] deleted");
+  return true
 });
