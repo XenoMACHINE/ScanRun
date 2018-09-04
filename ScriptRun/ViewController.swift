61f7ca0ee0f90b83c28388f75300350fed04d5af
Alexandre Ménielle <alexandre.menielle@gmail.com>
2018-09-04 12:57:21 +0200

    init project

---
+++ ScriptRun/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ScriptRun
+//
+//  Created by Alexandre Ménielle on 04/09/2018.
+//  Copyright © 2018 Alexandre Ménielle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
