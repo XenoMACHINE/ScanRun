bb7294a67d07a1b3c323d72b96f297b8c99be0b7
Alexandre Ménielle <alexandre.menielle@gmail.com>
2018-09-04 16:17:34 +0200

    test 1st function

--- Functions/functions/index.js
+++ Functions/functions/index.js
@@ -1,8 +1,13 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.autoCreateUser = functions.auth.user().onCreate(event => {
+
+  const user = event.data; // The Firebase user.
+  const email = user.email; // The email of the user.
+  const uid = user.uid;
+
+  return admin.firestore().document("users/"+uid+"email").set(email);
+  //return admin.database().ref("/users/"+uid+"/mail").set(email);
+});
